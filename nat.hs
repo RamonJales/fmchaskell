437f37d43f86768ab4d853a92379ef0718d7710e
RamonJales <ramonjales123@gmail.com>
2023-09-17 19:12:50 -0300

    sum, mult and exp functions implemented

--- nat.hs
+++ nat.hs
@@ -1,4 +1,17 @@
 module Nat where
+import Prelude hiding (sum, mult, exp, quot, min, gcd, lcm, div, max, pred, rem)
 
-data Nat = 0 | S Nat
-    deriving ( Eq , Show )
+data Nat = O | S Nat
+    deriving (Eq , Show)
+
+sum :: Nat -> Nat -> Nat
+sum n O = n
+sum n (S m) = S (sum n m)
+
+mult :: Nat -> Nat -> Nat
+mult n O = O
+mult n (S m) = sum (mult n m) n
+
+exp :: Nat -> Nat -> Nat
+exp n O = (S O)
+exp n (S m) = mult n (exp n m)
