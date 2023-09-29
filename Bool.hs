828bf236347cb3d147d658fd2b542f1cd17405c0
RamonJales <ramonjales123@gmail.com>
2023-09-29 10:23:29 -0300

    if_then_else function created

--- Bool.hs
+++ Bool.hs
@@ -4,3 +4,7 @@ import Prelude hiding (if_then_else)
 
 data Bool = False | True
     deriving ( Eq , Show )
+
+if_then_else :: Bool -> Nat -> Nat -> Nat
+if_then_else True n _ = n
+if_then_else False _ n = n
