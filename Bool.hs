f7493a69de02a91106d3eb1e22142bfd6cac3c47
RamonJales <ramonjales123@gmail.com>
2023-09-29 16:47:03 -0300

    equals function created

--- Bool.hs
+++ Bool.hs
@@ -1,6 +1,6 @@
 module Bool where
 import Nat
-import Prelude hiding (if_then_else)
+import Prelude hiding (if_then_else, leq, (==), False, True, Bool)
 
 data Bool = False | True
     deriving ( Eq , Show )
@@ -12,5 +12,12 @@ if_then_else False _ n = n
 leq ::  Nat -> Nat -> Bool
 leq O _ = True
 leq _ O = False
-leq (S n) (S m) = n m
+leq (S n) (S m) = leq n m
 
+(==) :: Nat-> Nat -> Bool
+O == O = True
+(S n) == (S m) = n == m
+_ == _ = False
+
+
+-- ev :: Nat -> Bool
