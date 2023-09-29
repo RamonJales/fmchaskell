aea7460bdfb5fa0bf0531558ef6353df1b337b0d
RamonJales <ramonjales123@gmail.com>
2023-09-29 16:32:28 -0300

    leq function created

--- Bool.hs
+++ Bool.hs
@@ -8,3 +8,9 @@ data Bool = False | True
 if_then_else :: Bool -> Nat -> Nat -> Nat
 if_then_else True n _ = n
 if_then_else False _ n = n
+
+leq ::  Nat -> Nat -> Bool
+leq O _ = True
+leq _ O = False
+leq (S n) (S m) = n m
+
