2daa0e1f13f88a27d024936cb318ce8bd02964eb
RamonJales <ramonjales123@gmail.com>
2023-10-01 17:29:38 -0300

    Length function created

--- ListNat.hs
+++ ListNat.hs
@@ -7,3 +7,7 @@ import Prelude hiding (Nil, length)
 data ListNat = Empty | Cons Nat ListNat
     deriving ( Eq , Show )
 
+
+length :: ListNat -> Nat
+length Empty = O
+length (Cons x xs) = S(length xs)
