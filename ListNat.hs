7440b6fda05a59c864b4ed7d03e6e76b59ac0e27
RamonJales <ramonjales123@gmail.com>
2023-10-01 22:02:13 -0300

    Product function created

--- ListNat.hs
+++ ListNat.hs
@@ -2,7 +2,7 @@ module ListNat where
 import Nat
 import Bool
 import Empty
-import Prelude hiding (Nil, length, sum)
+import Prelude hiding (Nil, length, sum, product)
 
 data ListNat = Empty | Cons Nat ListNat
     deriving ( Eq , Show )
@@ -15,3 +15,7 @@ length (Cons x xs) = S(length xs)
 sumList :: ListNat -> Nat
 sumList Empty = O
 sumList (Cons x xs) = sum x (sumList xs)
+
+product :: ListNat -> Nat
+product Empty = (S O)
+product (Cons x xs) = mult x (product xs)
