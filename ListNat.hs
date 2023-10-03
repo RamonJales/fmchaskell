fb7c09ca2d07e2fbf5799d481f7bdfec64451900
RamonJales <ramonjales123@gmail.com>
2023-10-03 16:11:00 -0300

    anyOdd function implemented

--- ListNat.hs
+++ ListNat.hs
@@ -45,3 +45,7 @@ anyEven (Cons x xs) = if_then_else_2 (ev x) True (anyEven xs)
 allOdd :: ListNat -> Bool
 allOdd Empty = True
 allOdd (Cons x xs) = if_then_else_2 (od x) (allOdd xs) False
+
+anyOdd :: ListNat -> Bool
+anyOdd Empty = False
+anyOdd (Cons x xs) = if_then_else_2 (od x) True (anyOdd xs)
