fff1b240beb851f569937aba80a717ec48baa2f4
RamonJales <ramonjales123@gmail.com>
2023-10-14 19:53:50 -0300

    map function added

--- List.hs
+++ List.hs
@@ -1,5 +1,5 @@
 module ListNat where
-import Prelude hiding(List)
+import Prelude hiding(List, map, filter)
 import Nat
 
 {-# LANGUAGE GADTs #-}
@@ -8,6 +8,10 @@ data List a where
     Empty :: List a
     Cons :: a -> List a -> List a
     deriving ( Eq , Show )
+
+map :: (a -> b) -> List a -> List b
+map f Empty = Empty
+map f (Cons x xs) = Cons (f x) (map f xs)
 
 append :: a -> List a -> List a
 append n Empty = Cons n Empty
