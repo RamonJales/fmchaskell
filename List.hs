cdd2585a536c1699cf0f9cc2e8ec425a2082f2d2
RamonJales <ramonjales123@gmail.com>
2023-10-14 14:15:17 -0300

    append function added

--- List.hs
+++ List.hs
@@ -8,3 +8,7 @@ data List a where
     Empty :: List a
     Cons :: a -> List a -> List a
     deriving ( Eq , Show )
+
+append :: a -> List a -> List a
+append n Empty = Cons n Empty
+append n (Cons x xs) = Cons x (append n xs)
