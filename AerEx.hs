b586640cafc0568b6cacdc047a38a81356ed9ca0
RamonJales <ramonjales123@gmail.com>
2024-01-28 00:55:30 -0300

    define tree operation type

---
+++ AerEx.hs
@@ -0,0 +1,15 @@
+-- module AerEx where 
+-- import Prelude
+-- import Dir
+
+-- type Path = [Dir]
+
+-- {-# LANGUAGE GADTs #-}
+
+-- data AEX Int Int where
+--     Tip :: Int -> AEX Int Int
+--     plus :: AEX Int Int -> AEX Int Int -> AEX Int Int
+--     times :: AEX Int Int -> AEX Int Int -> AEX Int Int
+--     deriving (Eq , Show)
+
+    
